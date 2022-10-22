4b354f3287fa8d2bf18bb9c8370ed8519ebf5f3f
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-10-22 03:49:20 +0300

    add_fetch_markup_notify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import fetchPhotos from './js/fetchPhotos.js';
 
-const DEBOUNCE_DELAY = 300;
+import markupGalleryCard from './template/gallery_card.hbs';
+
+const searhFormRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+
+searhFormRef.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  clearGallery();
+  const userInput = searhFormRef.elements.searchQuery.value.trim();
+  fetchPhotos(userInput).then(totalHits).then(appendMarkup);
+}
+function appendMarkup(data) {
+  console.log(data);
+  galleryRef.insertAdjacentHTML('beforeend', markupGalleryCard(data));
+}
+function clearGallery() {
+  galleryRef.innerHTML = '';
+}
+function totalHits(data) {
+  if (data.totalHits === 0) {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    Notify.info(`Hooray! We found ${data.totalHits} images.`);
+  }
+  return data;
+}
---
+++ src/js/fetchPhotos.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30721488-ec9ca19b7cca22464bcdf3786';
+
+export default async function fetchPhotos(userInput) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: KEY,
+        q: userInput,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: '40',
+      },
+    });
+    return await response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
